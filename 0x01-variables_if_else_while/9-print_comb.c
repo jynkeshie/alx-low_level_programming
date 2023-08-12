40539ea01cfdbd29d6630e5e8a0a3c1192c430c5
jynkeshie <jynkeshie@gmail.com>
2023-08-12 13:08:19 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 18; n < 28; n++)
+{
+putchar(n);
+
+if (n != 27)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
