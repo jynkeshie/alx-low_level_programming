f4872723a4ad92e0d25123f0f7ff3af4051f645b
jynkeshie <jynkeshie@gmail.com>
2023-08-15 18:58:34 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,9 +16,9 @@ void times_table(void)
 			if (z > 9)
 			{
 				u = z & 10;
-				d = z / 10;
-				_putchar(',');
-				_putchar(' ');
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
 				_putchar(d + '0');
 				_putchar(u + '0');
 			}
@@ -26,9 +26,9 @@ void times_table(void)
 			{
 				if (y != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
 				}
 				_putchar(z + '0');
 			}
