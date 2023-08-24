0d9c1f2584df3a098c54dc6a962a55138d0bb8c5
jynkeshie <jynkeshie@gmail.com>
2023-08-24 15:39:39 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,29 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * rev_string - reverse array
  * @n: integer params
  * Return: 0
  */
+
 void rev_string(char *n)
 {
 	int i = 0;
 	int j = 0;
 	char temp;
 
-	while (*(n) != '\0')
+	while (*(n + i) != '\0')
 	{
 		i++;
-		n++;
 	}
 	i--;
 
 	for (j = 0; j < i; j++, i--)
 	{
-		temp = *(n - j - 1);
-		*(n - j - 1) = *(n - i - 1);
-		*(n - i - 1) = temp;
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
@@ -41,49 +40,39 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
-		while (*(n1 + i) != '\0')
-			i++;
-		while (*(n2 + j) != '\0')
-			j++;
-		i--;
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
 		j--;
-
-		if (j > size_r || i >= size_r - 1)
-			return NULL;
-
-		while (j >= 0 || i >= 0 || overflow == 1)
-		{
-			if (i < 0)
-				val1 = 0;
-			else
-				val1 = *(n1 + i) - '0';
-			if (j < 0)
-				val2 = 0;
-			else
-				val2 = *(n2 + j) - '0';
-			
-			temp_tot = val1 + val2 + overflow;
-			if (overflow == 1)
-			{
-				temp_tot -= 10;
-				overflow = 0;
-			}
-			if (temp_tot >= 10)
-				overflow = 1;
-
-			if (digits >= size_r - 1)
-				return NULL;
-
-			*(r + digits) = (temp_tot % 10) + '0';
-			digits++;
-			j--;
-			i--;
-		}
-
-		if (digits >= size_r - 1)
-			return NULL;
-
-		*(r + digits) = '\0';
-		rev_string(r);
-		return (r);
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
